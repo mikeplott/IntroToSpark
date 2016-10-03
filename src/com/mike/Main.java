8a4e4993129ae411d3d8d6caa213c39158d62641
michaelplott <mike.plott80@gmail.com>
2016-10-03 11:40:30 -0400

    Initial push, intro to spark.

---
+++ src/com/mike/Main.java
@@ -0,0 +1,38 @@
+package com.mike;
+
+
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Spark.get(
+                "/",
+                (request, response) -> {
+                    HashMap m = new HashMap();
+                    m.put("name", "Mike");
+                    return new ModelAndView(m, "home.html");
+                },
+                new MustacheTemplateEngine()
+        );
+
+        Spark.get(
+                "/login",
+                (request, response) -> {
+                    return new ModelAndView(null, "login.html");
+                },
+                new MustacheTemplateEngine()
+        );
+
+        Spark.post(
+                "/login",
+                (request, response) -> {
+                    response.redirect("/");
+                    return null;
+                }
+        );
+    }
+}
